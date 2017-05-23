acf57ea2dfd26ced45cfd4502e94a6d1efcc6559
mohit.f2004760 <mohit.f2004760@gmail.com>
2017-05-23 10:50:31 +0530

    intermittent changes

--- api/db-data-generator.js
+++ api/db-data-generator.js
@@ -8,18 +8,22 @@ var i;
 var city = ["noida","surat","delhi","gurgaon","jalesar","patna","lucknow"];
 var areas = ["sec51","sec52","sec53","sec54","sec55","sec56","sec57"];
 var types = ["BB","BG","GOG","GOB","UC","FAM","OTH"];
-var cityStub, areaStub,typeStub;
-//********CAUTION BELOW LINE WILL REMOVE ALL USERS*******
+var names = ["Anand Shankar","Mohit Mittal", "Ashwani Kumar","Nilesh Rathi", "Pawan Singh", "Srajan Agrawal","Sakshya Agrawal","Sanjay Kumar"];
+var cityStub, areaStub,typeStub,username;
+
 cityStub = city[Math.floor(Math.random() * 8)];
 areaStub = areas[Math.floor(Math.random() * 8)];
 typeStub = types[Math.floor(Math.random() * 8)];
+username = names[Math.floor(Math.random() * 8)];
 
+//********CAUTION BELOW LINE WILL REMOVE ALL USERS*******
 db.users.remove({});
 db.users.insert({
 	"_id" : ObjectId("588c1f807d6893d132bee730"),
 	"userTypeOfTenant" : typeStub,
 	"userDesiredCity" : "'"+cityStub+"'",
-	"userFullName" : typeStub+"_"+cityStub+"_"+areaStub+"Mohit",
+	//"userFullName" : typeStub+"_"+cityStub+"_"+areaStub+"Mohit",
+    "userFullName" : username,
 	"userPassword" : "deadsea123",
 	"userEmail" : "0mittal@gmail.com",
 	"userNotifications" : [],
@@ -34,14 +38,16 @@ for (i = 1; i <= 1000; i++) {
    cityStub = city[Math.floor(Math.random() * 7)];
    areaStub = areas[Math.floor(Math.random() * 7)];
    typeStub = types[Math.floor(Math.random() * 7)];
-   
+   username = names[Math.floor(Math.random() * 7)]; 
+
    x = db.users.findOne();
    x._id = new ObjectId()
    x.userEmail = i+"mittal"+"@gmail.com";
    x.userDesiredCity = cityStub;
    x.userDesiredArea = areaStub;
    x.userTypeOfTenant= typeStub;
-   x.userFullName = typeStub+"_"+cityStub+"_"+areaStub+"Mohit";
+   //x.userFullName = typeStub+"_"+cityStub+"_"+areaStub+"Mohit";
+   x.userFullName = username;
    db.users.insert(x);
    
 }
